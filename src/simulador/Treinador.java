8c03f992e5320c2ef19c75b7fd80b35775458884
mac0321 25 <mac0321.25@metralha.cec.ime.usp.br>
2017-05-05 11:20:58 -0300

    First files

--- src/simulador/Treinador.java
+++ src/simulador/Treinador.java
@@ -1,5 +1,32 @@
 package simulador;
 
 public class Treinador {
-
+	public Pokemon[] pokemon = new Pokemon[6];
+	private String nome;
+	
+	public Treinador( String nome, Pokemon pokemon[]){
+		this.nome = nome;
+		this.pokemon = pokemon;
+	}
+	
+	void fugir(){
+		//acaba batalha e treinador oposto vence
+	}
+	Ataque escolheAtaque(Pokemon A){
+		// treinador escolhe um ataque entre os 4 ataques do pokemao A
+		//MUDAR PROXIMA LINHA DO RETURN
+		return null;
+	}
+	void trocar(Pokemon A, Pokemon B){
+		/*Pokemon Z = new Pokemon();
+		Z = A;
+		A = B;
+		B = Z;
+		Z = null;
+		*/
+	}
+	void usaritem(Pokemon A){
+		A.hp=100;
+		//hp max
+	}
 }
