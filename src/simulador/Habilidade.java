642779a56f11c3897e7f7d85e77e3781667f164c
Caio Augusto Pires <pirescaioauugusto@gmail.com>
2017-05-10 22:37:30 -0300

    atualizado

--- src/simulador/Habilidade.java
+++ src/simulador/Habilidade.java
@@ -4,15 +4,6 @@ public class Habilidade {
 
 	private String nome;
 	private int dano;
-<<<<<<< HEAD
-	private int tempo;
-	public Habilidade(String nome, int dano, int tempo){
-		this.nome = nome;
-		this.dano = dano;
-		this.tempo = tempo;
-	}
-	// menor velocidade mais rapido
-=======
 	private int velocidade;
 	public Habilidade(String nome, int dano, int velocidade){
 		this.nome = nome;
@@ -20,21 +11,14 @@ public class Habilidade {
 		this.velocidade = velocidade;
 	}
 	// maior velocidade mais rapido
->>>>>>> master
 	public String pegaNome(){
 		return this.nome;
 	}
 	public int pegaDano(){
 		return this.dano;
 	}
-<<<<<<< HEAD
-	public int pegaTempo(){
-		return this.tempo;
-	}
-}
-=======
+
 	public int pegaVelocidade(){
 		return this.velocidade;
 	}
 }
->>>>>>> master
